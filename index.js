acd4ac8483370148edd60f4aaa8533bd39c57999
Travis <tsahadeo@mac.com>
2021-09-24 17:52:29 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+main.remove();
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = "victory";
+newHeader.innerHTML = '<h1> "Travis is the champion" </h1>'
